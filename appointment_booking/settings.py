3155df960aa36d33d9b3a3788224933ec710eda2
Sandeep Kumar <9512sandy@gmail.com>
2026-04-20 17:01:55 +0530

    deploy

--- appointment_booking/settings.py
+++ appointment_booking/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-hwdh$16zzsz)7l%ts9!d2v7rko-5txj#!8z*i*0qqwmurf@np-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,7 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_URL = 'user_login'
 LOGIN_REDIRECT_URL = 'teacher_list'
